e94368f5e30ea374de5e1dda7f733c746b26297d
Vartika Vaish <2020uee0139@iitjammu.ac.in>
2023-01-19 23:34:25 +0530

    Time: 239 ms (5.00%), Space: 50.4 MB (5.26%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+ static bool comp (vector<int> p1, vector<int> p2) {
+    return p1[0] < p2[0] || (p1[0] == p2[0] && p1[1] < p2[1]);
+}
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> v;
+        stack<vector<int>> st;
+        sort(intervals.begin(),intervals.end(),comp);
+        st.push(intervals[0]);
+        for(int i=0;i<intervals.size();i++){
+            vector<int> top=st.top();
+            if(top[1]<intervals[i][0]){
+                st.push(intervals[i]);
+            }
+            else if(top[1]<intervals[i][1]){
+                top[1]=intervals[i][1];
+                st.pop();
+                st.push(top);
+                
+            }
+        }
+        while(!st.empty()){
+            v.push_back(st.top());
+            st.pop();
+        }
+        return v;
+        
+        
+    }
+};
