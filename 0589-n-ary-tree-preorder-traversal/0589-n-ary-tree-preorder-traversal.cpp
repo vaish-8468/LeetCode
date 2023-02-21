267855a8ca0c1f3c5c372fe24984741626849259
Vartika Vaish <2020uee0139@iitjammu.ac.in>
2023-02-21 21:50:52 +0530

    Time: 23 ms (67.53%), Space: 11.1 MB (80.42%) - LeetHub

---
+++ 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.cpp
@@ -0,0 +1,53 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+/* 1.Pop the top node from the stack – Top from the stack and insert it into the visited list of nodes.
+2. Push all of the child nodes of Top into the stack from right to left as the traversal from the stack will be carried out in reverse order. As a result, correct preorder traversal is achieved.*/
+
+class Solution {
+public:
+    void function(Node* root, vector<int> &v){
+    if(root==nullptr){
+        return;
+    }
+    v.push_back(root->val);
+    for(int i=0;i<root->children.size();i++){
+        function(root->children[i],v);
+    }
+}
+    vector<int> preorder(Node* root) {
+//         stack<Node*> st;
+        
+//         vector<int> preorder_;
+//         st.push(root);
+//         while(!st.empty()){
+//             Node* temp=st.top();
+//             st.pop();
+//              preorder_.push_back(temp->val);
+//             for(int i=temp->children.size()-1;i>=0;i--){
+//                 st.push(temp->children[i]);
+//             }
+//         }
+//      return preorder_;
+//     }
+    vector<int> v;
+    function(root,v);
+    return v;
+    }
+};
