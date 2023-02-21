bc7c937088e1c2f61362cff8c486e32412e07c2b
Vartika Vaish <2020uee0139@iitjammu.ac.in>
2023-02-21 21:51:13 +0530

    Time: 24 ms (63.88%), Space: 11.1 MB (92.61%) - LeetHub

--- 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.cpp
+++ 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.cpp
@@ -32,20 +32,6 @@ public:
     }
 }
     vector<int> preorder(Node* root) {
-//         stack<Node*> st;
-        
-//         vector<int> preorder_;
-//         st.push(root);
-//         while(!st.empty()){
-//             Node* temp=st.top();
-//             st.pop();
-//              preorder_.push_back(temp->val);
-//             for(int i=temp->children.size()-1;i>=0;i--){
-//                 st.push(temp->children[i]);
-//             }
-//         }
-//      return preorder_;
-//     }
     vector<int> v;
     function(root,v);
     return v;
