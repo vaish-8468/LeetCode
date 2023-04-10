9f3aa2eee49584e4d01be2483d9a23a56b95594c
Vartika Vaish <2020uee0139@iitjammu.ac.in>
2023-04-11 00:02:32 +0530

    Time: 4 ms (80.25%), Space: 10.8 MB (60.07%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,56 +1,29 @@
 class Solution {
 public:
     
-    // void combination(int index, vector<int>& candidates, vector<int>& v, vector<vector<int>>& ans, int target){
+    void combination(int index, vector<int>& candidates, vector<int>& v, vector<vector<int>>& ans, int target){
         
-//         if(index==candidates.size()){
-//             if(target==0){
-//                 ans.push_back(v);
-//                 // v.clear();
-//             }
-//             else{
-//                 return;
-//             }
-//         }
-//         if(candidates[index]<=target){
-//             //take
-//         v.push_back(candidates[index]);
-//         combination(index,candidates,v,ans,target-candidates[index]);
-//             //do-not take
-//         v.pop_back();
-//         }
-//         combination(index+1,candidates,v,ans,target);
-        
-//     }
-//     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-//         int n=candidates.size();
-//         vector<int> v;
-//         vector<vector<int>> ans;
-        
-//         combination(0,candidates,v,ans,target);
-//         return ans;
-        
-//     }
-    vector<vector<int>>ans;
-    void help(int i,int t,vector<int>&c,vector<int>&vec){
-        if(t==0){
-            ans.push_back(vec);
-            return;
+        if(target==0){
+            ans.push_back(v);
+        return;
         }
-        if(t<0){
-            return;
+        if(index==candidates.size()){
+          return;
         }
-        if(i==c.size()){
-            return;
+        if(target<0){
+            return ;
         }
-        help(i+1,t,c,vec); // if ith ele is ignored
-        vec.push_back(c[i]);
-        help(i,t-c[i],c,vec);// if ith ele is not ignored
-        vec.pop_back();
+        v.push_back(candidates[index]); //take
+        combination(index,candidates,v,ans,target-candidates[index]);
+        v.pop_back(); //do-not take
+        combination(index+1,candidates,v,ans,target);
+        
     }
-    vector<vector<int>> combinationSum(vector<int>& c, int t) {
-        vector<int>vec;
-        help(0,t,c,vec);
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        int n=candidates.size();
+        vector<int> v;
+        vector<vector<int>>ans;
+        combination(0,candidates,v,ans,target);
         return ans;
     }
 };
