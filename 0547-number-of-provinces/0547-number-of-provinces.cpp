65d846ddcb23fa3410d2a1b45f0ede380958c725
Vartika Vaish <2020uee0139@iitjammu.ac.in>
2023-06-09 23:20:36 +0530

    Time: 29 ms (43.46%), Space: 13.7 MB (79.34%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -5,8 +5,9 @@ public:
         visited[node]=1;
         
         for(int i=0; i<isConnected[node].size();i++){
-            if(isConnected[node][i]==1 && node!=i){
-                if(visited[i]==0){
+            if(isConnected[node][i]==1 && node!=i){ //node!=i is wriitn since the adjacenecy list contains self loop also
+                //dfs traversal for adjacenecy matrix
+                if(visited[i]==0){ //if it's connected node is unvisited , we call it recursively
                 dfs(i,isConnected,visited);
                     }
             }
@@ -19,7 +20,7 @@ public:
         for(int i=0;i<isConnected.size();i++){
             if(visited[i]==0){
                 dfs(i,isConnected,visited);
-                ans++;
+                ans++; //counts the number of coponents which will give number of provinces attached
             }
         }
         return ans;
