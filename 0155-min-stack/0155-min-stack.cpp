e8abd106897825e7ec0940cfa720322c1404b4a6
Vartika Vaish <2020uee0139@iitjammu.ac.in>
2023-06-13 22:45:57 +0530

    Time: 26 ms (47.78%), Space: 16.4 MB (36.31%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -2,8 +2,8 @@ class MinStack {
 public:
     
     int top_;
-    vector<int> min_ele;
-    int min_=INT_MAX;
+    vector<int> min_ele; //maintains the min_ele for every operation
+    int min_=INT_MAX;  //initial min element value
     vector<int> arr;
     MinStack() {
      top_=-1;
@@ -14,8 +14,8 @@ public:
     void push(int val) {
     arr.push_back(val);
         top_++;
-        min_ele.push_back(min(min_,val));
-        min_=min_ele.at(top_);
+        min_ele.push_back(min(min_,val));  //for every peartion, store the min_element
+        min_=min_ele.at(top_); //keep modifying the value of min_ simultaneously
         
         
     }
@@ -25,7 +25,7 @@ public:
         min_ele.pop_back();
         top_--;
         if(top_==-1){
-            min_=INT_MAX;
+            min_=INT_MAX;  //when the stack is empty
         }
         else{
         min_=min_ele.at(top_);
@@ -41,7 +41,7 @@ public:
     
     int getMin() {
         if(top_==-1){
-            return min_;
+            return min_; //when the stack is empty
         }
        return min_ele.at(top_);
     }
