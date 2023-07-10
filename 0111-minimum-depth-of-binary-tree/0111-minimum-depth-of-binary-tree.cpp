a39b775bffcdf9d9f0538ab84ebf6cfa3f00c56b
Vartika Vaish <2020uee0139@iitjammu.ac.in>
2023-07-10 23:23:14 +0530

    Time: 338 ms (5.40%), Space: 146.2 MB (8.83%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
@@ -23,6 +23,6 @@ public:
         int left= root->left==nullptr? INT_MAX : minDepth(root->left);  //if left child is null then take int_max else take its depth
         int right= root->right==nullptr? INT_MAX : minDepth(root->right); //if right child is null then take int_max else take its depth
         
-        return 1+min(left,right); //take 1+ minimum of the two traversals
-    }
+        return 1+min(left,right); //take 1+ minimum of the two traversals and no overflow since we have already mentioned the leaf node condition
+    } 
 };
