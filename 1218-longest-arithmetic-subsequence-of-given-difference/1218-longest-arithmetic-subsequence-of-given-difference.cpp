2c8d9f6433bffece669031a375ad39892b97f59b
Vartika Vaish <2020uee0139@iitjammu.ac.in>
2023-07-14 19:11:13 +0530

    Time: 321 ms (7.33%), Space: 57.4 MB (14.47%) - LeetHub

--- 1218-longest-arithmetic-subsequence-of-given-difference/1218-longest-arithmetic-subsequence-of-given-difference.cpp
+++ 1218-longest-arithmetic-subsequence-of-given-difference/1218-longest-arithmetic-subsequence-of-given-difference.cpp
@@ -8,12 +8,14 @@ public:
         int result=0;
         for(int i=0;i<n;i++){
             int prevState=arr[i]-difference; //store a-d element
-            if(mp.find(prevState)==mp.end()){ //if a-d doesn't exists then no previous state to ith element contribute in the subsequence
-                mp[arr[i]]=1; //count only the present element to be a part of the subsequence
-            }
-            else{ 
+            
+            // mp[prevState]
+            // if(mp.find(prevState)==mp.end()){ //if a-d doesn't exists then no previous state to ith element contribute in the subsequence
+            //     mp[arr[i]]=1; //count only the present element to be a part of the subsequence
+            // }
+            // else{ 
                 mp[arr[i]]=mp[prevState]+1; //otherwise add 1 to the length of the existng subsequence since the previous state has been found which contributes to the subsequence
-            }
+            // }
             
             result=max(result,mp[arr[i]]); //store the max of the result
         }
