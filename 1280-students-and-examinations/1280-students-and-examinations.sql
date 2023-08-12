d8735ef3b2e9e41ccb21c994d4602ec3591eee9e
Vartika Vaish <2020uee0139@iitjammu.ac.in>
2023-08-12 15:51:05 +0530

    Time: 2409 ms (17.43%), Space: 0B (100.00%) - LeetHub

--- 1280-students-and-examinations/1280-students-and-examinations.sql
+++ 1280-students-and-examinations/1280-students-and-examinations.sql
@@ -1,9 +1,9 @@
 # Write your MySQL query statement below
-SELECT Students.student_id,Students.student_name, Subjects.subject_name,  IFNULL(grouped.attended_exams,0) AS attended_exams
+SELECT Students.student_id,Students.student_name, Subjects.subject_name,  IFNULL(grouped.attended_exams_count,0) AS attended_exams
 FROM Students
 JOIN Subjects
 LEFT JOIN (
-SELECT student_id, subject_name, COUNT(*) AS attended_exams
+SELECT student_id, subject_name, COUNT(*) AS attended_exams_count
 FROM Examinations
 GROUP BY student_id, subject_name) grouped
 ON Students.student_id=grouped.student_id AND Subjects.subject_name=grouped.subject_name
