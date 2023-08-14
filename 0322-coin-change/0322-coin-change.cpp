b85f8aa139b8e14efb2a6916cd4dd16a89215f73
Vartika Vaish <2020uee0139@iitjammu.ac.in>
2023-08-14 10:54:53 +0530

    Time: 98 ms (63.41%), Space: 18.5 MB (45.33%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,22 +1,37 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-      int n=amount;
-       int dp[++n];
-        dp[0] = 0;
-        // more convenient to have the coins sorted
-        sort(begin(coins), end(coins));
-        // populating our dp array
-        for (int i = 1; i < n; i++) {
-            // setting dp[0] base value to 1, 0 for all the rest
-            dp[i] = INT_MAX;
-            for (int c: coins) {
-                if (i - c < 0) break;
-                // if it was a previously not reached cell, we do not add use it
-                if (dp[i - c] != INT_MAX) dp[i] = min(dp[i], 1 + dp[i - c]);
-            }
+        int ans=INT_MAX;
+      vector<int> prev(amount+1,0), curr(amount+1,0);
+      int n=coins.size();
+      for(int tar=0;tar<=amount;tar++){
+        if(tar%coins[0]==0){
+            prev[tar]=tar/coins[0];
         }
-        return dp[--n] == INT_MAX ? -1 : dp[n];
+          else{
+              prev[tar]=INT_MAX;
+          }
+      }
+        
+        for(int i=1;i<n;i++){
+            for(int tar=0;tar<=amount;tar++){
+                int notTake=0 + prev[tar];
+                
+                int take=INT_MAX;
+                if(coins[i]<=tar && curr[tar-coins[i]]!=INT_MAX){
+                    take=1+curr[tar-coins[i]];
+                }
+                
+                curr[tar]=min(take,notTake);
+            }
+            prev=curr;
+        }
+        
+        if(prev[amount]>=INT_MAX){
+            return -1;
+        }
+        
+        return prev[amount];
         
     }
 };
