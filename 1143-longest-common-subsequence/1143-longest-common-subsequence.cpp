daa91dd64dc61d799b9d86317357342eac545aa6
Vartika Vaish <2020uee0139@iitjammu.ac.in>
2023-08-15 14:49:43 +0530

    Time: 57 ms (63.83%), Space: 24.6 MB (27.11%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -8,7 +8,7 @@ public:
         
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
-            if(text1[i-1]==text2[j-1]){
+            if(text1[i-1]==text2[j-1]){ //since we've converted 0 based indexing into 1 based indexing
                 dp[i][j]=1+dp[i-1][j-1];
             }
             else{
