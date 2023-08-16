70bd13786042920cb1f45d74a80d23135b5a1713
Vartika Vaish <2020uee0139@iitjammu.ac.in>
2023-08-16 15:00:29 +0530

    Time: 73 ms (56.03%), Space: 28.6 MB (18.33%) - LeetHub

---
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minInsertions(string s) {
+        int n=s.length();
+        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+        
+        
+        //find the longest palindromic subsequence length 
+        
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(s[i-1]==s[n-j]){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        // to make the whole string palindromic we have  to insert the non-palindromic elemnts
+        //the min number of operations reuquired will be the count of those elements
+        int minSteps=n-dp[n][n];
+        
+        return minSteps;
+    }
+};
