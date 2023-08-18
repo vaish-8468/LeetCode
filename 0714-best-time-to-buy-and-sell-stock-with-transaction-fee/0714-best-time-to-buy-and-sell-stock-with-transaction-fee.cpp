2b7aae060e21c9aa190dad39996503a5315bc6bd
Vartika Vaish <2020uee0139@iitjammu.ac.in>
2023-08-18 14:13:47 +0530

    Time: 175 ms (43.39%), Space: 91 MB (30.54%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,22 +1,32 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices, int fee) {
-        int aheadnotbuy, aheadbuy, currbuy, currnotbuy;
-        aheadnotbuy=aheadbuy=0;
+        int n=prices.size();
         
-        for(int i=prices.size()-1;i>=0;i--){
-            //sell
-            
-            currnotbuy=max(prices[i]-fee+aheadbuy, 0+aheadnotbuy);
-            
-            //buy
-            
-            currbuy= max(-prices[i]+aheadnotbuy, 0+aheadbuy);
-            
-            aheadbuy=currbuy;
-            aheadnotbuy=currnotbuy;
+        vector<vector<int>> dp(n+1, vector<int>(2,0));
+        
+        //base case
+        //for all n, no profit will be counted as the araay has been fully traversed
+        
+        
+        for(int ind=n-1;ind>=0;ind--){
+            for(int buy=0;buy<=1;buy++){
+                int profit=0;
+                
+                if(buy){
+                    profit=max(-prices[ind]+dp[ind+1][0],      0+dp[ind+1][1]);
+                    //           bought    do not buy next    not bought  buy next
+                }
+                
+                else{
+                    profit=max(prices[ind]-fee+dp[ind+1][1], 0+dp[ind+1][0]);
+                      //        sold and completed one transaction so buy next   otherwise   not sold , then stock on hold , do not buy next
+                }
+                
+                dp[ind][buy]=profit;
+            }
         }
         
-        return aheadbuy;
+        return dp[0][1]; //when stock was bought
     }
 };
