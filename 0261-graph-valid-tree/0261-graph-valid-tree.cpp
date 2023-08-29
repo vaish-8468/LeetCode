e330a2a0047a904020d94c1825dee4477d765a7b
Vartika Vaish <2020uee0139@iitjammu.ac.in>
2023-08-29 20:45:16 +0530

    Time: 12 ms (89.31%), Space: 11.9 MB (95.32%) - LeetHub

---
+++ 0261-graph-valid-tree/0261-graph-valid-tree.cpp
@@ -0,0 +1,58 @@
+class UnionSet{
+    private: 
+    vector<int> root;
+    
+    public:
+    UnionSet(int size){
+        for(int i=0;i<size;i++){
+            root.push_back(i);
+        }
+    }
+    
+    int find(int node){
+        while(root[node]!=node){
+            node=root[node];
+        }
+        return node; //returns the root of the tree
+    }
+    
+    bool Union(int x, int y){
+        int rootX=find(x);
+        int rootY=find(y);
+        
+        if(rootX==rootY){
+            return false;
+        }
+        root[rootX]=rootY;
+        return true;
+    }
+};
+
+class Solution {
+public:
+    
+   
+    
+    bool validTree(int n, vector<vector<int>>& edges) {
+        //a graph is a valid tree if it is connected and contains no cycle in any case
+        
+        if(n-1!=edges.size()){
+            return false;
+        }
+        
+        UnionSet UniSet(n);
+        
+        //create an adjacency list
+ 
+        for(int i=0;i<edges.size();i++){
+            int u=edges[i][0];
+            int v=edges[i][1];
+            
+            if(!UniSet.Union(u,v)){
+                return false;
+            }
+        }
+        
+     return true;
+    }
+};
