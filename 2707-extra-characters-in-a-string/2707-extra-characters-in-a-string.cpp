fd9a85c6d9ff72bee533096257e9575fb3d44c4e
Vartika Vaish <2020uee0139@iitjammu.ac.in>
2023-09-05 23:11:03 +0530

    Time: 102 ms (72.79%), Space: 62.7 MB (83.00%) - LeetHub

--- 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.cpp
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.cpp
@@ -1,30 +1,47 @@
 class Solution {
 public:
-    int minExtraChar(string s, vector<string> dictionary) {
-        int n = s.length();
-        unordered_set<string> dictionarySet(dictionary.begin(), dictionary.end());
-        unordered_map<int, int> memo;
-
-        function<int(int)> dp = [&](int start) {
-            if (start == n) {
-                return 0;
-            }
-            if (memo.count(start)) {
-                return memo[start];
-            }
-            // To count this character as a left over character 
-            // move to index 'start + 1'
-            int ans = dp(start + 1) + 1;
-            for (int end = start; end < n; end++) {
-                auto curr = s.substr(start, end - start + 1);
-                if (dictionarySet.count(curr)) {
-                    ans = min(ans, dp(end + 1));
-                }
-            }
-
-            return memo[start] = ans;
-        };
-
-        return dp(0);
+  unordered_set<string> st;
+    int t[51];
+    
+    int n;
+    
+    int solve(int idx, string &s) {
+        
+        if(idx >= n)
+            return 0;
+        
+        if(t[idx] != -1)
+            return t[idx];
+        
+        string currStr = "";
+        int minExtra   = n;
+        
+        for(int i = idx; i<n; i++) {
+            
+            currStr.push_back(s[i]);
+            
+            int currExtra = (st.find(currStr) == st.end()) ? currStr.length() : 0;
+            
+            int nextExtra = solve(i+1, s);
+            
+            int totalExtra = currExtra + nextExtra;
+            
+            minExtra = min(minExtra, totalExtra);
+            
+        }
+        
+        return t[idx] = minExtra;
+        
+    }
+    
+    int minExtraChar(string s, vector<string>& dictionary) {
+        n = s.length();
+        
+        memset(t, -1, sizeof(t));
+        
+        for(string &word : dictionary)
+            st.insert(word);
+        
+        return solve(0, s);
     }
 };
