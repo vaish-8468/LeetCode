f015407c7c0d8e4f9dfceedf7fbc567bebd5f18b
Vartika Vaish <2020uee0139@iitjammu.ac.in>
2023-09-11 21:23:08 +0530

    Time: 15 ms (28.98%), Space: 13.6 MB (26.77%) - LeetHub

--- 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
@@ -3,15 +3,18 @@ public:
     vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
         vector<pair<int,int>> mp;
         vector<vector<int>> ans;
+        //make pair of indices along with their groupp size
         for(int i=0;i<groupSizes.size();i++){
             mp.push_back(make_pair(i,groupSizes[i]));
             
         }
         
+        //sort the vector according to the group sizes
         sort(mp.begin(),mp.end(),[&](const pair<int,int>& a, const pair<int,int>& b){
             return a.second<b.second;
         });
         
+        //start grouping the elements as per the group size
         for(int i=0;i<groupSizes.size();){
             int n=mp[i].second;
             vector<int> group(n);
