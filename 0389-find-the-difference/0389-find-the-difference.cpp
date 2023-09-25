04d3bbd30613ca513259edfce5d59fb36938a952
Vartika Vaish <2020uee0139@iitjammu.ac.in>
2023-09-25 20:57:57 +0530

    Time: 6 ms (19.69%), Space: 7.3 MB (8.68%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -5,14 +5,16 @@ public:
         
         for(auto i: s){
             st[i]++;
+            //adding the character and its freq into the map
         }
                                          
             char extra=' ';
         for(auto i:t){
             if(st.find(i)!=st.end() && st[i]!=0){
+                //deducting frequency of all those who are present in s
                 st[i]--;
             }
-            else{
+            else{ //when the char is not present in s or even though it's present but it does not hold any frequency in s, that will be our answer
                 extra=i;
             }
         }
