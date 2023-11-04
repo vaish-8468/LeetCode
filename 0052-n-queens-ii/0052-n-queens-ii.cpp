c7822b6bbffbc46157239f8ca58ef9d2734b7bff
Vartika Vaish <2020uee0139@iitjammu.ac.in>
2023-11-04 20:51:19 +0530

    Time: 6 ms (41.58%), Space: 7 MB (30.64%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    void placeQueens(int col, int n, vector<string>& board, set<vector<string>>& ans, vector<bool>&top_left, vector<bool>&left, vector<bool>& bottom_left){
+        if(col==n){
+            ans.insert(board);
+            return;
+        }
+        
+        for(int row=0;row<n;row++){
+            if(bottom_left[row+col]==false && left[row]==false && top_left[n-1+col-row]==false){
+                //place
+                board[row][col]='Q';
+                bottom_left[row+col]=true;
+                top_left[n-1+col-row]=true;
+                left[row]=true;
+                
+                placeQueens(col+1,n,board,ans,top_left,left,bottom_left);
+                
+                //backtrack
+                board[row][col]='.';
+                bottom_left[row+col]=false;
+                top_left[n-1+col-row]=false;
+                left[row]=false;
+                
+            }
+        }
+    }
+    int totalNQueens(int n) {
+        set<vector<string>> ans;
+        string s(n,'.');
+        vector<string> board(n,s);
+        vector<bool> top_left(2*n-1,false), bottom_left(2*n-1,false), left(n,false);
+        placeQueens(0,n,board,ans,top_left,left,bottom_left);
+        
+        return ans.size();
+        
+    }
+};
