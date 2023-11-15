7ecd12ae9231e5ee8cfbfc8d9ed7830434cf4268
Vartika Vaish <2020uee0139@iitjammu.ac.in>
2023-11-15 23:21:22 +0530

    Time: 77 ms (64.55%), Space: 51.6 MB (56.72%) - LeetHub

---
+++ 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
+        int n=arr.size();
+        int maxi=-1;
+        
+        sort(arr.begin(),arr.end());
+        
+        for(int i=0;i<n;i++){
+            if(i==0){
+                arr[i]=1;
+            }
+            else if(abs(arr[i]-arr[i-1])>1){
+                arr[i]=arr[i-1]+1;
+            }
+            maxi=max(arr[i],maxi);
+        }
+        
+        return maxi;
+    }
+};
