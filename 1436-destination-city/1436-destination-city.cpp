e348124648dffb63aa5f1086d350b6f5709ff89e
Vartika Vaish <2020uee0139@iitjammu.ac.in>
2023-12-15 12:47:53 +0530

    Time: 8 ms (75.96%), Space: 10.8 MB (85.89%) - LeetHub

--- 1436-destination-city/1436-destination-city.cpp
+++ 1436-destination-city/1436-destination-city.cpp
@@ -1,24 +1,46 @@
 class Solution {
 public:
     string destCity(vector<vector<string>>& paths) {
-        unordered_map<string,vector<string>> adj;
-        set<string> st;
-        string ans;
-        for(int i=0;i<paths.size();i++){
-            string u=paths[i][0];
-            string v=paths[i][1];
-            st.insert(u);
-            st.insert(v);
-            adj[u].push_back(v);
+        unordered_set<string> hasOutgoing;
+        for (int i = 0; i < paths.size(); i++) {
+            hasOutgoing.insert(paths[i][0]);
         }
         
-        for(auto x: st){
-            if(adj.find(x)==adj.end()){
-                ans=x;
-                break;
+        for (int i = 0; i < paths.size(); i++) {
+            string candidate = paths[i][1];
+            if (hasOutgoing.find(candidate) == hasOutgoing.end()) {
+                return candidate;
             }
         }
         
-        return ans;
+        return "";
     }
 };
+
+
+
+
+// class Solution {
+// public:
+//     string destCity(vector<vector<string>>& paths) {
+//         unordered_map<string,vector<string>> adj;
+//         set<string> st;
+//         string ans;
+//         for(int i=0;i<paths.size();i++){
+//             string u=paths[i][0];
+//             string v=paths[i][1];
+//             st.insert(u);
+//             st.insert(v);
+//             adj[u].push_back(v);
+//         }
+        
+//         for(auto x: st){
+//             if(adj.find(x)==adj.end()){
+//                 ans=x;
+//                 break;
+//             }
+//         }
+        
+//         return ans;
+//     }
+// };
