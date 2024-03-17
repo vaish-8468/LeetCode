8161592f69569bde0ed2d5083a129cec7984993f
Vartika Vaish <2020uee0139@iitjammu.ac.in>
2024-03-18 01:29:43 +0530

    Time: 8 ms (81.58%), Space: 20.3 MB (84.54%) - LeetHub

---
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        vector<vector<int>> res;
+        int n=intervals.size();
+        int i=0;
+        for( i=0;i<n;i++){
+            if(intervals[i][1]<newInterval[0]){
+                res.push_back(intervals[i]);
+            }
+            else if(intervals[i][0]>newInterval[1]){
+                break;
+                //no overalapping anymore
+            }
+            else{
+                //overlapping case--->merge and update
+                newInterval[0]=min(newInterval[0],intervals[i][0]);
+                newInterval[1]=max(newInterval[1],intervals[i][1]);
+            }
+        }
+        
+        res.push_back(newInterval);
+        while(i<n){
+            res.push_back(intervals[i]);
+            i++;
+        }
+        
+        
+        
+        return res;
+    }
+};
