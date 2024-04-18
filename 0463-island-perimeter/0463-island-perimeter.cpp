feaf1e9f9db5b58524c9ab33e61a42ffefefebfe
Vartika Vaish <2020uee0139@iitjammu.ac.in>
2024-04-18 15:16:14 +0530

    Time: 78 ms (44.92%), Space: 100.8 MB (76.64%) - LeetHub

---
+++ 0463-island-perimeter/0463-island-perimeter.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int islandPerimeter(vector<vector<int>>& grid) {
+        //count only those blocks which has water atleast on one of its side.
+        int dx[4]={0,-1,0,1};
+        int dy[4]={-1,0,1,0};
+        int m=grid.size();
+        int n=grid[0].size();
+        int perimeter=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==1){
+                    for(int k=0;k<4;k++){
+                        int x=i+dx[k];
+                        int y=j+dy[k];
+                        if(x>-1 && x<m && y>-1 && y<n){
+                            if(grid[x][y]==0){
+                                perimeter+=1;
+                            }
+                        }
+                        else{
+                            perimeter+=1;
+                        }
+                    }
+                }
+            }
+        }
+        return perimeter;
+    }
+};
