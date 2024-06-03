c42aabc6c25e8fd079ebb21073e01f8d06e04008
Vartika Vaish <2020uee0139@iitjammu.ac.in>
2024-06-03 11:36:36 +0800

    Time: 19 ms (59.76%), Space: 11.9 MB (47.32%) - LeetHub

--- 2486-append-characters-to-string-to-make-subsequence/2486-append-characters-to-string-to-make-subsequence.cpp
+++ 2486-append-characters-to-string-to-make-subsequence/2486-append-characters-to-string-to-make-subsequence.cpp
@@ -10,15 +10,18 @@ public:
         
         while(i<n && j<m){
             if(s[i]==t[j]){
+                //increment both i and j pointer
                 i++;
                 j++;
             }
             else{
+                //otherwise increment only i pointer in order to find the occurence of the next character of t in s
                 i++;
             }
         }
         
-       count=m-j;
+       count=m-j; //count the remaining characters of t left to be appended in s
+        //if m==j, signifies that all characters of t exist in s
         
         return count;
     }
