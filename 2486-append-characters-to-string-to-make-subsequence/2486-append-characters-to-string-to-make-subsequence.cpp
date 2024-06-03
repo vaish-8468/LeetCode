1da2a865dfbe654d28f2b2754f9d5ee0dd415a79
Vartika Vaish <2020uee0139@iitjammu.ac.in>
2024-06-03 11:34:16 +0800

    Time: 18 ms (67.70%), Space: 11.9 MB (83.42%) - LeetHub

---
+++ 2486-append-characters-to-string-to-make-subsequence/2486-append-characters-to-string-to-make-subsequence.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int appendCharacters(string s, string t) {
+        int i=0;
+        int j=0;
+        int n=s.length();
+        int m=t.length();
+        
+        int count=0;
+        
+        while(i<n && j<m){
+            if(s[i]==t[j]){
+                i++;
+                j++;
+            }
+            else{
+                i++;
+            }
+        }
+        
+       count=m-j;
+        
+        return count;
+    }
+};
