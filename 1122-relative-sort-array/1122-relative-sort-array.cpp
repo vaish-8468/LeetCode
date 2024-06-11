17c414ebb911b7478c50bb28ce7fc634f74fb25d
Vartika Vaish <2020uee0139@iitjammu.ac.in>
2024-06-11 12:31:51 +0800

    Time: 0 ms (100.00%), Space: 10.2 MB (41.93%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.cpp
+++ 1122-relative-sort-array/1122-relative-sort-array.cpp
@@ -14,22 +14,23 @@ public:
         int x=n-1;
         for(int i=n-1;i>=0;i--){
             if(mp.find(arr1[i])!=mp.end()){
-                mp[arr1[i]]++;
+                mp[arr1[i]]++;//counting the frequency of arr2 elements in arr1
             }
             else{
-                vec.push_back(arr1[i]);
+                vec.push_back(arr1[i]); //storing arr1 elements which are not in arr2
                 // arr1[x--]=arr1[i];
             }
         }
         sort(vec.begin(),vec.end());
+        //sort the vec
         
         for(int i=vec.size()-1;i>=0;i--){
-            arr1[x--]=vec[i];
+            arr1[x--]=vec[i]; //first store the sorted elements uncommon to arr2
         }
        
         for(int i=m-1;i>=0;i--){
             while(mp[arr2[i]]--){
-                arr1[x--]=arr2[i];
+                arr1[x--]=arr2[i]; //store all the known elements of arr2
             }
         }
         
